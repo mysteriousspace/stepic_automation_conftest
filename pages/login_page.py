61832457d0947b5ac851b0f3edf506afe75d1c36
Павел Коньков <pkonkov@mmtr.ru>
2021-07-26 12:13:13 +0300

    Page Object and Exception

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        url = self.browser.current_url
+        assert url.find("login") != -1, "this is not login page"
+
+    def should_be_login_form(self):
+        self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
+        assert True, "There is no LogIn form"
+
+    def should_be_register_form(self):
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM)
+        assert True, "There is no Registration form"
